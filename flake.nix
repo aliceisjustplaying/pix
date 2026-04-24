8fa08876c1a40744f6b1fd473ed79b2a16fdc194
alice <aliceisjustplaying@gmail.com>
2026-04-24 16:31:09 +0100

    hmm2

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-22_05.url = "github:NixOS/nixpkgs/nixos-22.05";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
