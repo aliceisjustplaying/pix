06ac2fa5123a54c9254077dc2f21f6daa60637a0
alice <aliceisjustplaying@gmail.com>
2026-05-03 11:38:29 +0000

    Pin tsshd 0.1.7

--- pkgs/tsshd.nix
+++ pkgs/tsshd.nix
@@ -1,15 +1,28 @@
-{ symlinkJoin, makeWrapper, tsshd }:
+{ symlinkJoin, makeWrapper, tsshd, fetchFromGitHub }:
 
 let
   # Pinned UDP range so the firewall on tailscale0 can allow exactly this.
   portRange = "61000-61999";
+  pinnedTsshd = tsshd.overrideAttrs (finalAttrs: _oldAttrs: {
+    version = "0.1.7";
+
+    src = fetchFromGitHub {
+      owner = "trzsz";
+      repo = "tsshd";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-9llfXzAAQgAOeaD+o3AVyhP0uL88uQsCNlqAPNfzDVw=";
+    };
+
+    vendorHash = "sha256-btTWkuLkT2e58TYqe0e/cE/0Try/g8XoahiABSSFaGU=";
+    checkFlags = [ "-skip=^TestQUIC_" ];
+  });
 in
 symlinkJoin {
-  name = "tsshd-wrapped-${tsshd.version}";
-  paths = [ tsshd ];
+  name = "tsshd-wrapped-${pinnedTsshd.version}";
+  paths = [ pinnedTsshd ];
   nativeBuildInputs = [ makeWrapper ];
   postBuild = ''
     wrapProgram $out/bin/tsshd --add-flags "--port ${portRange}"
   '';
-  meta = tsshd.meta;
+  meta = pinnedTsshd.meta;
 }
