895b8e265a3920aa07e3c221083b873e831d4171
alice <aliceisjustplaying@gmail.com>
2026-06-12 20:56:31 +0000

    Update signal-cli to 0.14.5

--- pkgs/signal-cli.nix
+++ pkgs/signal-cli.nix
@@ -4,10 +4,10 @@
 }:
 
 signal-cli.overrideAttrs (_old: rec {
-  version = "0.14.4.1";
+  version = "0.14.5";
 
   src = fetchurl {
     url = "https://github.com/AsamK/signal-cli/releases/download/v${version}/signal-cli-${version}.tar.gz";
-    hash = "sha256-nLDYG20UOrihpcNRpv6KWRMDEIw3KccTKy5MEjID1X8=";
+    hash = "sha256-YtOOv+85iNePQ35zKBg7de5UnRETguZsGvcNPr0816c=";
   };
 })
